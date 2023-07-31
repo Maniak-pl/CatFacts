58599a80d5520032b9138a1f31d5df951eb68b85
Piotr <android@core-logic.com>
2023-07-31 05:36:58 +0200

    13. Dependency Injection done

--- app/src/main/java/pl/maniak/catfacts/presentation/MainActivity.kt
+++ app/src/main/java/pl/maniak/catfacts/presentation/MainActivity.kt
@@ -5,9 +5,17 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import pl.maniak.catfacts.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +26,30 @@ class MainActivity : AppCompatActivity() {
         val errorView = findViewById<TextView>(R.id.errorView)
         val catFactView = findViewById<TextView>(R.id.catFactView)
 
+        val catFactRepository = di.catFactRepository
         getFactButton.setOnClickListener {
-
+            disposable = catFactRepository.getFact()
+                .toObservable()
+                .onErrorResumeNext(Observable.empty())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    loadingIndicator.isVisible = true
+                }
+                .doOnError {
+                    loadingIndicator.isVisible = false
+                    errorView.isVisible = true
+                }
+                .subscribe { fact ->
+                    loadingIndicator.isVisible = false
+                    catFactView.text = fact
+                    errorView.isVisible = false
+                }
         }
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
     }
 }
